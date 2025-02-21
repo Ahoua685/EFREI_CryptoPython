1dd41b59aade8774a4e0057667895a1e8a7b8da3
Ahoua685 <ahouacool@gmail.com>
2025-02-21 16:51:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,25 +11,39 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-@app.route('/generate_key')
 def generate_key():
     key = Fernet.generate_key()
-    return f"Votre clé personnelle : {key.decode}"
+    return key.decode()
 
 
-@app.route('/encrypt/<string:custom_key>/<string:valeur>')
-def encryptage(custom_key,valeur):
+@app.route('/encrypt', methods=['POST'])
+def encryptage():
+    data = request.get_json()  
+    custom_key = data.get('key')
+    valeur = data.get('valeur')
+
+    
+    if not custom_key:
+        custom_key = generate_key()
+
     f = Fernet(custom_key.encode())
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
-    token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode}"  # Retourne le token en str
+    token = f.encrypt(valeur_bytes)  # Chiffrement de la valeur
 
-@app.route('/decrypt/<string:custom_key>/<string:token>')
-def decryptage(custom_key,token):
-    f = Fernet(custom_key.encode())
-    token_bytes = token.encode()
-    valeur_dechiffree = f.decrypt(token_bytes)
-    return f"Valeur décryptée : {valeur_dechiffree.decode}"
+    return jsonify({'key': custom_key, 'valeur_encryptee': token.decode()})
+
+
+@app.route('/decrypt', methods=['POST'])
+def decryptage():
+    data = request.get_json() 
+    custom_key = data.get('key')
+    token = data.get('token')
+
+    f = Fernet(custom_key.encode())  
+    token_bytes = token.encode()  
+    valeur_dechiffree = f.decrypt(token_bytes)  
+
+    return jsonify({'valeur_decryptee': valeur_dechiffree.decode()})
                                                                                                                                                      
 if __name__ == "__main__":
   app.run(debug=True)
