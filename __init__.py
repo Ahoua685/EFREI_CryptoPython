fb6a0bc48070d8a37bec2e8c1c66c92437fdb1ae
Ahoua685 <ahouacool@gmail.com>
2025-02-21 15:25:35 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,17 +11,22 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-key = Fernet.generate_key()
-f = Fernet(key)
+@app.route('/generate_key')
+def generate_key():
+    key = Fernet.generate_key()
+    return f"Votre clé personnelle : {key.decode}"
 
-@app.route('/encrypt/<string:valeur>')
-def encryptage(valeur): 
+
+@app.route('/encrypt/<string:custuom_key>/<string:valeur>')
+def encryptage(custom_key,valeur):
+    f = Fernet(custom_key.encode())
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
 
-@app.route('/decrypt/<string:token>')
-def decryptage(token):
+@app.route('/decrypt/<string:custom_key>/<string:token>')
+def decryptage(custom_key,token):
+    f = Fernet(custom_key.encode())
     token_bytes = token.encode()
     valeur_dechiffree = f.decrypt(token_bytes)
     return f"Valeur décryptée : {valeur_dechiffree.decode()}"
