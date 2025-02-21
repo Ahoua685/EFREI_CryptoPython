b77dbc6871e6e55018eea96ff5e4674926aab03c
Ahoua685 <ahouacool@gmail.com>
2025-02-21 18:04:09 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from cryptography.fernet import Fernet 
-from flask import Flask, render_template_string, render_template, request, jsonify
+from flask import Flask, render_template_string, render_template, jsonify, request, json
 from flask import render_template
-from flask import json
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
